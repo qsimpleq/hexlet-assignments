ef85634c3227b3c7b84132ea99dd59b45b3f7bd4
@hexlet/cli <support@hexlet.io>
2023-06-04 15:45:31 +0500

    submit rails-basics-ru/quality

--- rails-basics-ru/quality/config/routes.rb
+++ rails-basics-ru/quality/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'bulletins#index'
   # BEGIN
   resources :bulletins, only: %w[index show]
   # END
--- rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -1,11 +1,26 @@
 require 'test_helper'
 
-class BulletinsControllerTest < ActiveSupport::TestCase
+class BulletinsControllerTest < ActionDispatch::IntegrationTest
   test 'show Bulletins index' do
     bulletin1 = bulletins('bulletin_1')
     assert bulletin1.id == 1
     assert bulletin1.title == 'Bulletin 1'
     assert bulletin1.body == 'Bulletin Body 1'
     assert bulletin1.published == true
+
+    get root_path
+    assert_response :success
+    assert_select 'h1', 'Bulletins'
+    assert_select 'h2', 'Bulletin 1'
+    assert_select 'p', 'Bulletin Body 1'
+  end
+
+  test 'show Bulletin 1' do
+    bulletin1 = bulletins('bulletin_1')
+
+    get bulletin_path(bulletin1)
+    assert_response :success
+    assert_select 'h1', 'Bulletin 1'
+    assert_select 'p', 'Bulletin Body 1'
   end
 end
