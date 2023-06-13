9978ebe00daa92931e5629b025c251c118873aee
@hexlet/cli <support@hexlet.io>
2023-06-13 17:28:49 +0500

    submit rails-basics-ru/middlewares

---
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class SetLocaleMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  # BEGIN
+  def call(env)
+    dup._call(env)
+  end
+
+  def _call(env)
+    locale = env['HTTP_ACCEPT_LANGUAGE']&.downcase&.to_sym
+    I18n.locale = if !locale.nil? && I18n.available_locales.include?(locale)
+                    locale
+                  else
+                    I18n.default_locale
+                  end
+
+    @app.call(env)
+  end
+  # END
+end
