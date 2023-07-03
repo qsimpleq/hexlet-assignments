d54cd0543d2c13eeaf39f44920fd711d0b18c1e4
@hexlet/cli <support@hexlet.io>
2023-07-03 15:38:46 +0500

    submit rails-real-ru/files

---
+++ rails-real-ru/files/app/models/vehicle.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Vehicle < ApplicationRecord
+  # BEGIN
+  has_one_attached :image
+  # END
+
+  validates :manufacture, presence: true
+  validates :model, presence: true
+  validates :color, presence: true
+  validates :doors, presence: true, numericality: { only_integer: true }
+  validates :kilometrage, presence: true, numericality: { only_integer: true }
+  validates :production_year, presence: true
+
+  # BEGIN
+  validates :image, presence:true, content_type: %i[png jpg jpeg], size: { less_than: 5.megabytes }
+  # END
+end
